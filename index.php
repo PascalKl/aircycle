699cbcb86d7ef67ce7a2a1f9127d182ba33a86db
PascalKl <pascal.klamroth@gmail.com>
2023-02-05 22:10:56 +0100

    2

--- index.php
+++ index.php
@@ -4,13 +4,8 @@
     include 'functions.php';
 
 
-?><script language="javascript" type="text/javascript" >
-    function pull(){
-        var msg = <?php git_pull(); ?>;
-        alert("Test: " + msg);
-    }
-</script>
-<button onclick="pull()">Pull from GitHub</button>
+?>
+<button onclick="<?php git_pull(); ?>">Pull from GitHub</button>
 <?php
 
 ?>
