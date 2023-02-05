b91f1544531ad8b425d28b50c1c6ad9f4674b275
PascalKl <pascal.klamroth@gmail.com>
2023-02-05 19:30:13 +0100

    test

--- functions.php
+++ functions.php
@@ -1,7 +1,16 @@
 <?php 
 function git_pull(){
     try{
-        shell_exec("git pull");
+        include('Net/SSH2.php');
+
+    $ssh = new Net_SSH2('www.domain.tld');
+    $ssh->login('admin', 'Passi2003');
+
+    $ssh->read('[prompt]');
+    $ssh->write("sudo git pull\n");
+    $ssh->read('Password:');
+    $ssh->write("Passi2003\n");
+    echo $ssh->read('[prompt]');
         return "Pulled from GitHub";
     }
     catch(Excepetion $e){
